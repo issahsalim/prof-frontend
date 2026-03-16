4173578dac9bda107ef20e5d085cd5a5fb270664
boresa <issahsalim233@gmail.com>
2026-03-16 11:33:46 +0000

    connected backend

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': { target: 'http://127.0.0.1:8000/', changeOrigin: true },
-      '/media': { target: 'http://127.0.0.1:8000/', changeOrigin: true },
+      '/api': { target: 'https://profelvisasarebediako.onrender.com/', changeOrigin: true },
+      '/media': { target: 'https://profelvisasarebediako.onrender.com/', changeOrigin: true },
     },
   },
 }) 
